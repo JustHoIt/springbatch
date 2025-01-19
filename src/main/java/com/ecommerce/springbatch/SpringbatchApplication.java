24c8687988837d6819b8cebda3be041d95127a64
Just_Ho_it <zalop3128@kakao.com>
2025-01-20 00:20:09 +0900

    개발환경 구성

--- src/main/java/com/ecommerce/springbatch/SpringbatchApplication.java
+++ src/main/java/com/ecommerce/springbatch/SpringbatchApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringbatchApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringbatchApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(SpringbatchApplication.class, args);
+  }
 
 }
